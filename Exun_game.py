e88308f75cc62ec0157172904e3c9e25b8115b58
NimanshJ <97619527+NimanshJ@users.noreply.github.com>
2022-11-10 01:49:41 +0530

    Update Exun_game.py

--- Exun_game.py
+++ Exun_game.py
@@ -21,9 +21,9 @@ music_button = pygame.Rect(1175, 38, 90, 90)
 music = True
 
 start_button = pygame.Rect(531, 368, 237, 96)
-finish = [[1240, 540], [1240, 600], [1240, 600], [1240, 600], [1240, 600]]
-level = 0
-score_font = pygame.font.Font(None, 20)
+finish = [[1240, 540], [1240, 600], [1240, 600]]
+level = 3
+score_font = pygame.font.Font(None, 80)
 start_img = pygame.image.load("start.png")
 finish_img = pygame.image.load("finish.png")
 start_img = pygame.transform.scale(start_img,(screenw, screenh)) 
@@ -31,44 +31,48 @@ finish_img = pygame.transform.scale(finish_img,(screenw, screenh))
 
 backgrounds = [[pygame.image.load("bg1.png")],
                 [pygame.image.load("bg1.png")],
-                [pygame.image.load("bg1.png")],
-                [pygame.image.load("bg1.png")],
-                [pygame.image.load("bg1.png")],]
+                [pygame.image.load("bg1.png")]]
 
-for i in range(5):
+for i in range(3):
     backgrounds[i][0] = pygame.transform.scale(backgrounds[i][0],(screenw, screenh))   
 
 obstacles_levels = [[[pygame.Rect(1050, 500, 140, 60), (100, 56, 25)],
                     [pygame.Rect(130, 470, 140, 60), (100, 56, 25)],
                     [pygame.Rect(350, 380, 140, 160), (100, 56, 25)],
-                    [pygame.Rect(620, 340, 140, 60), (100, 56, 25)],
+                    [pygame.Rect(600, 340, 140, 60), (100, 56, 25)],
                     [pygame.Rect(0, screenh-90, screenw, 20), (33, 197, 67)]],
-                    [pygame.Rect(0, screenh-20, screenw, 20)],
-                    [pygame.Rect(0, screenh-20, screenw, 20)],
-                    [pygame.Rect(0, screenh-20, screenw, 20)],
-                    [pygame.Rect(0, screenh-20, screenw, 20)]]
+                    [[pygame.Rect(0, screenh-90, screenw, 20), (33, 197, 67)],
+                    [pygame.Rect(730, 220, 160, 40), (100, 56, 25)]],
+                    [[pygame.Rect(0, screenh-90, screenw, 20), (33, 197, 67)],
+                    [pygame.Rect(70, 140, 15, 380), (100, 56, 25)],
+                    [pygame.Rect(70, 220, 140, 20), (100, 56, 25)],
+                    [pygame.Rect(70, 400, 140, 20), (100, 56, 25)],
+                    [pygame.Rect(290, 140, 15, 425), (100, 56, 25)],
+                    [pygame.Rect(230, 130, 140, 20), (100, 56, 25)],
+                    [pygame.Rect(160, 310, 140, 20), (100, 56, 25)],
+                    [pygame.Rect(160, 480, 140, 20), (100, 56, 25)],
+                    [pygame.Rect(1050, 120, 140, 15), (100, 56, 25)],
+                    [pygame.Rect(370, 350, 140, 20), (100, 56, 25)],]]
 acid_lakes_levels = [[[pygame.Rect(540, 530, 330, 30), (255, 0, 0)]],
-                    [],
-                    [],
-                    [],
-                    []]
+                    [[pygame.Rect(160, 530, 1000, 30), (255, 0, 0)]],
+                    [[pygame.Rect(350, 530, 890, 30), (255, 0, 0)]]]
 
 move_platforms_levels = [[[pygame.Rect(780, 450, 100, 10), 900, 780]],
-                        [],
-                        [],
-                        [],
-                        []]
+                        [[pygame.Rect(900, 350, 100, 10), 1100, 900]],
+                        [[pygame.Rect(550, 250, 100, 10), 1100, 550],
+                        [pygame.Rect(550, 450, 100, 10), 1100, 550]]]
 return1 = False
 
 temp_platforms_levels = [[],
-                        [],
-                        [],
-                        [],
-                        []]
+                        [pygame.Rect(110, 500, 150, 20),
+                        pygame.Rect(320, 400, 150, 20),
+                        pygame.Rect(520, 300, 150, 20)],
+                        [pygame.Rect(450, 120, 125, 15),
+                        pygame.Rect(640, 120, 125, 15),
+                        pygame.Rect(830, 120, 125, 15)]]
 
-spritesp1 = [pygame.image.load("sprite1.png"),
-            pygame.image.load("sprite2.png")]
-
+spritesp1 = pygame.image.load("char1.png")
+spritesp1 = pygame.transform.scale(spritesp1,(30, 30))
 startx, starty = 0, 0
 rightp1 = False
 leftp1 = False
@@ -83,8 +87,8 @@ onsurfacep1 = False
 countp1 = 0
 pointsp1 = 0
 
-spritesp2 = [pygame.image.load("sprite1.png"),
-            pygame.image.load("sprite2.png")]
+spritesp2 = pygame.image.load("char2.png")
+spritesp2 = pygame.transform.scale(spritesp2,(30, 30))
 rightp2 = False
 leftp2 = False
 jumpp2 = False
@@ -108,6 +112,10 @@ start = False
 while run:
     screen.fill((0, 0, 0))
     if start:
+        if level > 2:
+            start = False
+            finish_screen = True
+            continue
         screen.blit(backgrounds[level][0], (0,0))
         score_text = score_font.render(f'{pointsp1} - {pointsp2}', True, (255, 255, 255))
         screen.blit(score_text, textRect)
@@ -121,22 +129,10 @@ while run:
         move_platforms = move_platforms_levels[level]
         temp_platforms = temp_platforms_levels[level]
 
-        if (p1temp > 0) and (p1temp <= 10):
-            p1i = 0
-        elif (p1temp > 10) and (p1temp <= 20):
-            p1i = 1
-        elif p1temp > 20:
-            p1temp = 0
-        imagep1 = spritesp1[p1i]
+        imagep1 = spritesp1
         screen.blit(imagep1, (p1x, p1y))
 
-        if (p2temp > 0) and (p2temp <= 10):
-            p2i = 0
-        elif (p2temp > 10) and (p2temp <= 20):
-            p2i = 1
-        elif p2temp > 20:
-            p2temp = 0
-        imagep2 = spritesp2[p2i]
+        imagep2 = spritesp2
         screen.blit(imagep2, (p2x, p2y))
 
         sizep1 = imagep1.get_size()
@@ -186,14 +182,14 @@ while run:
                 print("done")
 
         for a in move_platforms:
-            pygame.draw.rect(screen, (255, 255, 255), a[0])
+            pygame.draw.rect(screen, (255, 255, 0), a[0])
             if return1:
                 if a[0].x > a[2]:
-                    a[0].x -= 1
+                    a[0].x -= 3
                 else:
                     return1 = False
             elif a[0].x < a[1]:
-                a[0].x += 1
+                a[0].x += 3
             elif a[0].x >= a[1]:
                 return1 = True
 
@@ -212,7 +208,7 @@ while run:
                 jumpp2 = False
 
         for b in temp_platforms:
-            pygame.draw.rect(screen, (255, 255, 255), b)
+            pygame.draw.rect(screen, (0, 0, 255), b)
             if p1y == b.bottom and (p1x > b.left-sizep1[0]) and (p1x < b.right):
                 p1y = b.bottom
             if p2y == b.bottom and (p2x > b.left-sizep2[0]) and (p2x < b.right):
@@ -386,12 +382,30 @@ while run:
 
 
     elif finish_screen:
-        screen.fill((255, 255, 255))
+        pygame.draw.rect(screen, (255, 255, 255), music_button)
         screen.blit(finish_img, (0,0))
+        win_font = pygame.font.Font(None, 120)
+        winner = ""
+        if pointsp1 > pointsp2:
+            winner = "Red"
+        else:
+            winner = "Blue"
+        win_msg = win_font.render(f"{winner} Wins!!", True, (255, 255, 255))
+        textRect1 = win_msg.get_rect()
+        textRect1.center = (screenw//2, 200)
+        screen.blit(win_msg, textRect1)    
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
                 sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if music_button.collidepoint(event.pos):
+                    if music:
+                        mixer.music.pause()
+                        music = False
+                    else:
+                        mixer.music.unpause()
+                        music = True
 
 
     else:
